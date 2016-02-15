93a9e9112ac4d43d7b888ad93ee871083a0d5712
Radu Tanasescu <radu.tanasescu@gmail.com>
2016-02-15 23:32:03 +0000

    added usernames to chat

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var nicknames = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.sockets.on('connection', function(socket){
+  socket.on('new user', function(data, callback){
+    if (nicknames.indexOf(data) != -1){
+      callback(false);
+    } else{
+      callback(true);
+      console.log(data + ' has connected...');
+      socket.nickname = data;
+      nicknames.push(socket.nickname);
+    }
+  });
+
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+  });
+  
+  socket.on('disconnect', function(data){
+    if(!socket.nickname) return;
+    nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+    console.log(socket.nickname + ' has disconnected...');
   });
 });
 
