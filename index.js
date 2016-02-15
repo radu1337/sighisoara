a4589902379d14e0c320297101697c1244609a99
Radu Tanasescu <radu.tanasescu@gmail.com>
2016-02-15 20:10:56 +0000

    removed bootstrap grid and styled chat box

--- index.js
+++ index.js
@@ -4,18 +4,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-  });
-});
-
-io.on('connection', function(socket){
-  socket.on('set tile', function(tileParam){
-    io.emit('set tile', tileParam);
   });
 });
 
--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,16 +1,14 @@
 var socket = io();
+
+console.log(socket);
+
 $('form#chat').submit(function(){
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
+  socket.emit('chat message', $('#mess').val());
+  $('#mess').val('');
   return false;
 });
 
-var tileParam = {top:"road", righ:"land", bottom:"castle", left:"castle"};
-
-// set tile
-$('#clicker').click(function() {
-  socket.emit('set tile', tileParam);
-});
+// set username
 
 //send message
 socket.on('chat message', function(msg){
@@ -20,3 +18,34 @@ socket.on('chat message', function(msg){
 socket.on('set tile', function(tileParams){
   console.log(tileParams);
 });
+
+// keep input focused
+
+$("#chat").on("mouseup", function () {
+    $('input#mess', this).focus();
+});
+
+// tiles and tile sellection
+
+var tiles = [
+  { id:      "001",
+    top:     "castle",
+    right:   "field",
+    bottom:  "field",
+    left:    "castle"
+  },
+  { id:      "002",
+    top:     "field",
+    right:   "road",
+    bottom:  "field",
+    left:    "road"
+  },
+  { id:      "003",
+    top:     "castle",
+    right:   "field",
+    bottom:  "road",
+    left:    "field"
+  }
+];
+
+var randomTile = tiles[Math.floor(Math.random()*tiles.length)];
