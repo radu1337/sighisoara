4e08da8eebf553ddf2ac34e25cd6d90a358e7b3f
Radu Tanasescu <radu.tanasescu@gmail.com>
2016-02-16 06:27:01 +0000

    moved tile array to backend

--- index.js
+++ index.js
@@ -5,6 +5,53 @@ var io = require('socket.io')(http);
 
 var nicknames = [];
 
+// data
+
+var tiles = [
+  { id:      '000',
+    top:     'castle',
+    right:   'road',
+    bottom:  'field',
+    left:    'road'
+  },
+  { id:      '001',
+    top:     'castle',
+    right:   'field',
+    bottom:  'field',
+    left:    'castle'
+  },
+  { id:      '002',
+    top:     'field',
+    right:   'field',
+    bottom:  'road',
+    left:    'field'
+  },
+  { id:      '003',
+    top:     'castle',
+    right:   'road',
+    bottom:  'road',
+    left:    'field'
+  },
+  { id:      '004',
+    top:     'field',
+    right:   'castle',
+    bottom:  'field',
+    left:    'castle'
+  },
+  { id:      '005',
+    top:     'field',
+    right:   'field',
+    bottom:  'road',
+    left:    'road'
+  },
+  { id:      '006',
+    top:     'road',
+    right:   'road',
+    bottom:  'road',
+    left:    'road'
+  }
+];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -19,6 +66,12 @@ io.sockets.on('connection', function(socket){
       socket.nickname = data;
       nicknames.push(socket.nickname);
     }
+  });
+
+  socket.on('sellect random tile', function (){
+    var randomTile = tiles[Math.floor(Math.random()*tiles.length)];
+    console.log(randomTile);
+    io.sockets.emit('new tile', randomTile);
   });
 
   socket.on('send message', function(data){
--- public/js/frontend.js
+++ public/js/frontend.js
@@ -2,6 +2,11 @@ var socket = io.connect();
 
 //console.log(socket);
 
+// functions
+var getRandomTile = function(){
+  socket.emit('sellect random tile');
+}
+
 // set username
 $('form#submit_username').submit(function(e){
   e.preventDefault();
@@ -10,7 +15,7 @@ $('form#submit_username').submit(function(e){
     if(data){
       $('#username_box').hide();
       $('#chat, #game').show();
-      sellectTile();
+      getRandomTile();
     } else{
       $('#username').val('');
     }
@@ -28,71 +33,18 @@ socket.on('new message', function(data){
   $('#messages').append('<li><strong>' + data.nick + ':</strong> ' + data.msg + '</li>');
 });
 
-// data
-
-var tiles = [
-  { id:      '000',
-    top:     'castle',
-    right:   'road',
-    bottom:  'field',
-    left:    'road'
-  },
-  { id:      '001',
-    top:     'castle',
-    right:   'field',
-    bottom:  'field',
-    left:    'castle'
-  },
-  { id:      '002',
-    top:     'field',
-    right:   'field',
-    bottom:  'road',
-    left:    'field'
-  },
-  { id:      '003',
-    top:     'castle',
-    right:   'road',
-    bottom:  'road',
-    left:    'field'
-  },
-  { id:      '004',
-    top:     'field',
-    right:   'castle',
-    bottom:  'field',
-    left:    'castle'
-  },
-  { id:      '005',
-    top:     'field',
-    right:   'field',
-    bottom:  'road',
-    left:    'road'
-  },
-  { id:      '006',
-    top:     'road',
-    right:   'road',
-    bottom:  'road',
-    left:    'road'
-  }
-];
-
-// functions
-
-var sellectTile = function() {
-  var randomTile = tiles[Math.floor(Math.random()*tiles.length)];
-
-  $('#game').html('<img src=\"images/tile_' +  randomTile.id + '.png\" alt=\"Tile 001\" />');
-
-  //console.log(randomTile);
-};
+socket.on('new tile', function(data){
+  $('#game').html('<img src=\"images/tile_' +  data.id + '.png\" alt=\"Tile 001\" />');
+});
 
 // actions
 
-$('#chat').on('mouseup', function () {
+$('#chat').on('mouseup', function() {
   $('input#mess', this).focus();
 });
 
 $('#game').on('mousedown', function(){
-  sellectTile();
+  getRandomTile();
 });
 
 
